629d054fc4610da42151bb8548449a15a0945cd9
Daniil Zavyalov <dezavv1@gmail.com>
2023-06-18 18:24:37 +0600

    new project

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.messagingrabbitmq;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MessagingRabbitmqApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
